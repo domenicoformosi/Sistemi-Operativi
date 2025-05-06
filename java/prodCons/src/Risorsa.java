844042f714fa11231ec0b47d5d98f208932c5119
domenicoformosi <domenico.formosi@studio.unibo.it>
2025-05-06 23:18:32 +0200

    bho ho fatto delle robe non so cosa

--- java/prodCons/src/Risorsa.java
+++ java/prodCons/src/Risorsa.java
@@ -1,5 +1,5 @@
 import java.util.concurrent.Semaphore;
-public class risorsa {
+public class Risorsa {
 // definizione buffer condiviso:
 final int N = 30; // capacità del buffer
 int lettura, scrittura;//indice di lettura
@@ -7,7 +7,7 @@ int []buffer;
 Semaphore sP; /* sospensione dei Produttori; v.i. N*/
 Semaphore sC; /* sospensione dei Consumatori v.i. 0*/
 Semaphore sM1, sM2; // semafori di mutua esclus. v.i. 1
-public risorsa() // costruttore
+public Risorsa() // costruttore
 { lettura=0;
 scrittura=0;
 buffer= new int[N];
---
+++ java/prodCons/src/threadC.java
@@ -0,0 +1,14 @@
+public class threadC extends Thread{ //consumatori
+int msg;
+Risorsa r;
+public threadC(Risorsa R)
+{ this.r=R;
+}
+public void run()
+{ System.out.print("\nThread CONSUMATORE: il mioID è: "+getName()+"..\n");
+while (true)
+{ msg=r.prelievo();
+System.out.print("\n"+getName()+"consumatore ha letto il messaggio "+msg+ "...\n");
+}
+}
+}
